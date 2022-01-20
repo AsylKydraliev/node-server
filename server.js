508b2ddfbdf5ce7a3d4c40069e20197619a7f78c
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-01-20 18:39:24 +0600

    Задание-2.

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-    res.send('<h1>My page</h1>');
+app.get('/:name', (req, res) => {
+    res.send(`<h1>${req.params.name}</h1>`);
 });
 
-app.get('/Hello', (req, res) => {
-    res.send('<h1>Hello</h1>');
+app.listen(port, () => {
+    console.log('We are live on ' + port);
 });
-
-app.listen(port);
--- server2.js
+++ server2.js
@@ -1,5 +1,18 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const password = 'password';
 
-app.listen(port);
+const Vigenere = require('caesar-salad').Vigenere;
+
+app.get('/encode/:password', (req, res) => {
+    res.send(Vigenere.Cipher(password).crypt(req.params.password));
+});
+
+app.get('/decode/:password', (req, res) => {
+    res.send(Vigenere.Decipher(password).crypt(req.params.password));
+});
+
+app.listen(port, () => {
+    console.log('We are live on ' + port);
+});
